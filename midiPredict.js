e7710e667f390f29292222198f753b376fd5031f
aleathwick <leathwick@gmail.com>
2021-01-10 16:51:17 +1300

    loading midi files successfully

--- midiPredict.js
+++ midiPredict.js
@@ -1,8 +1,32 @@
+//  console.log(Object.keys(selectedFile));
+
+const validExtensions = ['mid', 'midi']
+fInfo = document.getElementById('fInfo')
+
 function fileReceived() {
- const selectedFile = this.files[0];
- document.getElementById('fInfo').innerHTML=selectedFile.name;
- console.log('loaded file')
- console.log(Object.keys(selectedFile));
+    const selectedFile = this.files[0];
+    if (validExtensions.indexOf(selectedFile.name.split('.').pop()) >= 0) {
+        
+        const reader = new FileReader();
+        midiBin = reader.readAsBinaryString(selectedFile);
+        const midi = new Midi(midiBin);
+
+        midi.tracks.forEach(track => {
+            //tracks have notes and controlChanges
+          
+            //notes are an array
+            const notes = track.notes;
+            notes.forEach(note => {
+                fInfo.innerHTML = 'failed?';
+                fInfo.innerHTML = note.name;
+              //note.midi, note.time, note.duration, note.name
+            })
+        })
+
+
+    } else {
+        alert(`The file extension ${selectedFile.name.split('.').pop()} is invalid, must be in ${validExtensions}`)
+    }
 }
 
 const inputElement = document.getElementById("input");
